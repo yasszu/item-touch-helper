b47fa4b1f043f4e3dabfe7476b3fd9a236bdc716
Yaushiro Suzuki <yasszu@yahoo.com>
2021-03-27 13:53:43 +0900

    fix gradle

--- app/src/main/java/com/example/todoapp/data/TasksRepository.kt
+++ app/src/main/java/com/example/todoapp/data/TasksRepository.kt
@@ -8,17 +8,25 @@ import com.example.todoapp.util.DateUtil
  */
 object TasksRepository {
 
+    private val sampleDate = arrayOf(
+            Task(id = 1, title = "TASK1", description = "description", date = DateUtil.currentDate),
+            Task(id = 2, title = "TASK2", description = "description", date = DateUtil.currentDate),
+            Task(id = 3, title = "TASK3", description = "description", date = DateUtil.currentDate),
+            Task(id = 4, title = "TASK4", description = "description", date = DateUtil.currentDate),
+            Task(id = 5, title = "TASK5", description = "description", date = DateUtil.currentDate),
+            Task(id = 6, title = "TASK6", description = "description", date = DateUtil.currentDate),
+            Task(id = 7, title = "TASK7", description = "description", date = DateUtil.currentDate),
+            Task(id = 8, title = "TASK8", description = "description", date = DateUtil.currentDate),
+            Task(id = 9, title = "TASK9", description = "description", date = DateUtil.currentDate)
+    )
+
+    private var tasks: ArrayList<Task> = arrayListOf()
+
+    init {
+        tasks.addAll(sampleDate)
+    }
+
     fun getTasks(): List<Task> {
-        val tasks = arrayListOf<Task>()
-        tasks.add(Task(id = 1, title = "TASK1", description = "description", date = DateUtil.currentDate))
-        tasks.add(Task(id = 2, title = "TASK2", description = "description", date = DateUtil.currentDate))
-        tasks.add(Task(id = 3, title = "TASK3", description = "description", date = DateUtil.currentDate))
-        tasks.add(Task(id = 4, title = "TASK4", description = "description", date = DateUtil.currentDate))
-        tasks.add(Task(id = 5, title = "TASK5", description = "description", date = DateUtil.currentDate))
-        tasks.add(Task(id = 6, title = "TASK6", description = "description", date = DateUtil.currentDate))
-        tasks.add(Task(id = 7, title = "TASK7", description = "description", date = DateUtil.currentDate))
-        tasks.add(Task(id = 8, title = "TASK8", description = "description", date = DateUtil.currentDate))
-        tasks.add(Task(id = 9, title = "TASK9", description = "description", date = DateUtil.currentDate))
         return tasks
     }
 
